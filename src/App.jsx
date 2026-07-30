ceddc459a99805f21b17e5708c7ce5a09686a671
c-mfy <xinyusu06@gmail.com>
2026-07-30 14:47:39 -0400

    Fixed mobile scroll

--- src/App.jsx
+++ src/App.jsx
@@ -20,22 +20,27 @@ export default function App() {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.1,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: "vertical",
       smoothWheel: true,
+      smoothTouch: true,
+      touchMultiplier: 1.5,
       wheelMultiplier: 1,
-      touchMultiplier: 2,
       infinite: false,
     });
 
+    let rafId;
+
     function raf(time) {
       lenis.raf(time);
-      requestAnimationFrame(raf);
+      rafId = requestAnimationFrame(raf);
     }
 
-    requestAnimationFrame(raf);
+    rafId = requestAnimationFrame(raf);
 
     return () => {
+      cancelAnimationFrame(rafId);
       lenis.destroy();
     };
   }, []);
